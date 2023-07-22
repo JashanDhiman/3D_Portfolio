19233a72139241b6979b23f534e92c87f817fc8d
JashanDhiman <jashandhiman.hulk@gmail.com>
2023-07-22 16:48:32 +0530

    changes of content

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,6 +23,8 @@ import {
     jobit,
     tripguide,
     threejs,
+    cueblocks,
+    smartdata,
 } from "../assets";
 
 export const navLinks = [
@@ -46,7 +48,7 @@ const services = [
         icon: web,
     },
     {
-        title: "React Native Developer",
+        title: "Shopify Custom Apps Developer",
         icon: mobile,
     },
     {
@@ -109,64 +111,93 @@ const technologies = [
         icon: figma,
     },
     {
-        name: "docker",
-        icon: docker,
+        name: "shopify",
+        icon: shopify,
     },
+    //{
+    //    name: "docker",
+    //    icon: docker,
+    //},
 ];
 
 const experiences = [
     {
-        title: "React.js Developer",
-        company_name: "Starbucks",
-        icon: starbucks,
-        iconBg: "#383E56",
-        date: "March 2020 - April 2021",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "React Native Developer",
-        company_name: "Tesla",
-        icon: tesla,
-        iconBg: "#E6DEDD",
-        date: "Jan 2021 - Feb 2022",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
         title: "Web Developer",
-        company_name: "Shopify",
-        icon: shopify,
+        company_name: "Cue Blocks",
+        icon: cueblocks,
         iconBg: "#383E56",
-        date: "Jan 2022 - Jan 2023",
+        date: "feb 2022 - sep 2022",
         points: [
             "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
             "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
+            "Together with my senior colleagues, I have successfully developed a customized Shopify app that aims to provide our customers with exceptional additional services and elevate their overall shopping experience.",
+            "This app not only enhances existing projects but also offers a more productive, customized and efficient code to customer's Store.",
         ],
     },
     {
         title: "Full stack Developer",
-        company_name: "Meta",
-        icon: meta,
-        iconBg: "#E6DEDD",
+        company_name: "SmartData",
+        icon: smartdata,
+        iconBg: "#383E56",
         date: "Jan 2023 - Present",
         points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Developing and maintaining Medical Platforms using React.js and integrated other third technologies such as Zoom SDK.",
+            "Collaborating with cross-functional teams including designers, project managers, and other developers to create high-quality Medical platforms.",
             "Participating in code reviews and providing constructive feedback to other developers.",
         ],
     },
+    //{
+    //    title: "React.js Developer",
+    //    company_name: "Starbucks",
+    //    icon: starbucks,
+    //    iconBg: "#383E56",
+    //    date: "March 2020 - April 2021",
+    //    points: [
+    //        "Developing and maintaining web applications using React.js and other related technologies.",
+    //        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //        "Implementing responsive design and ensuring cross-browser compatibility.",
+    //        "Participating in code reviews and providing constructive feedback to other developers.",
+    //    ],
+    //},
+    //{
+    //    title: "React Native Developer",
+    //    company_name: "Tesla",
+    //    icon: tesla,
+    //    iconBg: "#E6DEDD",
+    //    date: "Jan 2021 - Feb 2022",
+    //    points: [
+    //        "Developing and maintaining web applications using React.js and other related technologies.",
+    //        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //        "Implementing responsive design and ensuring cross-browser compatibility.",
+    //        "Participating in code reviews and providing constructive feedback to other developers.",
+    //    ],
+    //},
+    //{
+    //    title: "Web Developer",
+    //    company_name: "Shopify",
+    //    icon: shopify,
+    //    iconBg: "#383E56",
+    //    date: "Jan 2022 - Jan 2023",
+    //    points: [
+    //        "Developing and maintaining web applications using React.js and other related technologies.",
+    //        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //        "Implementing responsive design and ensuring cross-browser compatibility.",
+    //        "Participating in code reviews and providing constructive feedback to other developers.",
+    //    ],
+    //},
+    //{
+    //    title: "Full stack Developer",
+    //    company_name: "Meta",
+    //    icon: meta,
+    //    iconBg: "#E6DEDD",
+    //    date: "Jan 2023 - Present",
+    //    points: [
+    //        "Developing and maintaining web applications using React.js and other related technologies.",
+    //        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //        "Implementing responsive design and ensuring cross-browser compatibility.",
+    //        "Participating in code reviews and providing constructive feedback to other developers.",
+    //    ],
+    //},
 ];
 
 const testimonials = [
