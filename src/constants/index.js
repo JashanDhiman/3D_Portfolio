55ab4e6fe5315259cad9c534c2e6f7610e6846af
JashanDhiman <jashandhiman.hulk@gmail.com>
2025-12-13 22:44:05 +0530

    Changes in projects link and details.

--- src/constants/index.js
+++ src/constants/index.js
@@ -48,16 +48,16 @@ const services = [
         icon: web,
     },
     {
-        title: "Shopify Custom Apps Developer",
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Healthcare Platforms",
+      icon: creator,
+    },
+    {
+        title: "Shopify Custom Apps",
         icon: mobile,
-    },
-    {
-        title: "Backend Developer",
-        icon: backend,
-    },
-    {
-        title: "Content Creator",
-        icon: creator,
     },
 ];
 
@@ -98,10 +98,10 @@ const technologies = [
         name: "MongoDB",
         icon: mongodb,
     },
-    {
-        name: "Three JS",
-        icon: threejs,
-    },
+    //{
+    //    name: "Three JS",
+    //    icon: threejs,
+    //},
     {
         name: "git",
         icon: git,
@@ -141,8 +141,10 @@ const experiences = [
         iconBg: "#383E56",
         date: "Jan 2023 - Present",
         points: [
-            "Developing and maintaining Medical Platforms using React.js and integrated other third technologies such as Zoom SDK.",
-            "Collaborating with cross-functional teams including designers, project managers, and other developers to create high-quality Medical platforms.",
+            "Developing and maintaining Healthcare Platforms using React.js and nodeJS with taking-care of FHIR, HIPAA and PHI.",
+            "Optimizing application for maximum speed and scalability.",
+            "Developing Front-end with server-side logic.",
+            "Collaborating with cross-functional teams including designers, project managers, and other developers to create high-quality Healthcare platforms.",
             "Participating in code reviews and providing constructive feedback to other developers.",
         ],
     },
@@ -203,42 +205,42 @@ const experiences = [
 const testimonials = [
     {
         testimonial:
-            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-        name: "Sara Lee",
-        designation: "CFO",
-        company: "Acme Co",
+            "He is a talented guy, and intially I thought this will be difficult but then gradually we were able to turn this sound applicaiton to reality. Thanks to Jashan for being patient and keep working with me for 2 years.",
+        name: "Suneet Joshi",
+        designation: "Founder",
+        company: "NirvanaSage",
         image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
-    {
-        testimonial:
-            "I've never met a web developer who truly cares about their clients' success like Rick does.",
-        name: "Chris Brown",
-        designation: "COO",
-        company: "DEF Corp",
-        image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-        testimonial:
-            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-        name: "Lisa Wang",
-        designation: "CTO",
-        company: "456 Enterprises",
-        image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
+    //{
+    //    testimonial:
+    //        "I've never met a web developer who truly cares about their clients' success like Jashan does.",
+    //    name: "Chris Brown",
+    //    designation: "COO",
+    //    company: "DEF Corp",
+    //    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    //},
+    //{
+    //    testimonial:
+    //        "After Jashan optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    //    name: "Lisa Wang",
+    //    designation: "CTO",
+    //    company: "456 Enterprises",
+    //    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    //},
 ];
 
 const projects = [
     {
-        name: "Car Rent",
+        name: "Chat Web App",
         description:
-            "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+            "Web-based platform that allows users to login through google, create chat rooms, and chat with other users. Experience end-2-end encryption chating with us.",
         tags: [
             {
                 name: "react",
                 color: "blue-text-gradient",
             },
             {
-                name: "mongodb",
+                name: "firebase",
                 color: "green-text-gradient",
             },
             {
@@ -247,12 +249,13 @@ const projects = [
             },
         ],
         image: carrent,
+        link: "https://chat-web-app-36298.web.app/",
         source_code_link: "https://github.com/",
     },
     {
-        name: "Job IT",
+        name: "Memories timeline",
         description:
-            "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+            "Web application that helps users to revisit the memories of past with a beautiful journey of sailing boat on the river. By uploading images, users can experience this timeline.",
         tags: [
             {
                 name: "react",
@@ -263,33 +266,33 @@ const projects = [
                 color: "green-text-gradient",
             },
             {
-                name: "scss",
+                name: "gsap",
                 color: "pink-text-gradient",
             },
         ],
         image: jobit,
-        source_code_link: "https://github.com/",
+        source_code_link: "https://github.com/JashanDhiman/memories-timeline",
     },
     {
-        name: "Trip Guide",
+        name: "Sound healer",
         description:
-            "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+            "Web Application that helps users to improve there mental state, or chanalise the blocked area of brain via listening sounds, which are customised for each user specific.",
         tags: [
             {
-                name: "nextjs",
+                name: "react",
                 color: "blue-text-gradient",
             },
             {
-                name: "supabase",
+                name: "restapi",
                 color: "green-text-gradient",
             },
             {
-                name: "css",
+                name: "audioapi",
                 color: "pink-text-gradient",
             },
         ],
         image: tripguide,
-        source_code_link: "https://github.com/",
+        source_code_link: null,
     },
 ];
 
